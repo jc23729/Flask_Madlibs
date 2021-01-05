abcd336595779410fa8f17910408438ad68f85e0
Juan Cruz <jc23729@gmail.com>
2021-01-04 21:19:44 -0500

    setup requirements.txt file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 from stories import story
 
