6a446f36261d87b9835b647a295f44b27c28f45c
Juan Cruz <jc23729@gmail.com>
2021-01-04 21:11:19 -0500

    added basic flask app
    setup venv folder, and basic imports for the tools that we will be using

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+debug = DebugToolbarExtension(app)
+
